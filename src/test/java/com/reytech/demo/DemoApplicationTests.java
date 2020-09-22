f54d2de3da3dee4deb3203154018bc96c45385ee
Ms-27 <ms@juliver.com>
2020-09-22 09:54:28 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/reytech/demo/DemoApplicationTests.java
+++ src/test/java/com/reytech/demo/DemoApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-//modif pour build	
+//modif pour build in jenkins	
 	@Test
 	void contextLoads() {
 	}
