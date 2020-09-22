741ac41c23e9daa5975e116a2332e04d217f8521
Ms-27 <ms@juliver.com>
2020-09-22 11:47:11 +0200

    Update StandardAssertionsTest.java

--- src/test/java/com/reytech/demo/StandardAssertionsTest.java
+++ src/test/java/com/reytech/demo/StandardAssertionsTest.java
@@ -19,7 +19,7 @@ class StandardAssertionsTest {
     @Test
     void standardAssertions() {
     	
-        assertEquals(2, 2);
+        assertEquals(2, 5);
         assertNotEquals(2, 3);
         
         // affiche le message si le test ne passe pas
