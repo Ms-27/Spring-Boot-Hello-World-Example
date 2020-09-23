f1d89dc96d0fffb2d9ca33c53165c8a5da33e90f
Ms-27 <ms@juliver.com>
2020-09-23 09:36:54 +0200

    Update DemoApplication.java

--- src/main/java/com/reytech/demo/DemoApplication.java
+++ src/main/java/com/reytech/demo/DemoApplication.java
@@ -7,6 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 	private String nom = "12";
 	
+	public static void findbugDoesNotTrigger(List<String> object) {
+	    String string1 = object.get(0);
+	    String string2 = "AnyString";
+	    if(string1 == string2) {
+		System.out.println("Does not matter at all");   
+	    }
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
