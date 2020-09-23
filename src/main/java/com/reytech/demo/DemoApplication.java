c18ac2c07652a711822c2e421b2ad6f3738be136
Ms-27 <ms@juliver.com>
2020-09-23 12:09:25 +0200

    Update DemoApplication.java

--- src/main/java/com/reytech/demo/DemoApplication.java
+++ src/main/java/com/reytech/demo/DemoApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	
+	private static final long serialVersionUID = 1905162041950251407L;
+	
+	private Set<Integer> mySet;      // Findbugs error
+	
+	private HashSet<Integer> myOtherSet;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
